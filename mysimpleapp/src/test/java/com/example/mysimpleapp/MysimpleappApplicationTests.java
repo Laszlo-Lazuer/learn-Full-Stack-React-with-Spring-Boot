665da12faa2557089e6dfbe22c86c037099fd023
Laszlo Lazuer <visualjazz@gmail.com>
2026-03-27 21:00:25 -0600

    chore: updated app

---
+++ mysimpleapp/src/test/java/com/example/mysimpleapp/MysimpleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysimpleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysimpleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
